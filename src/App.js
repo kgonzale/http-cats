18b6605dd09c1b087875b7f92c30bf2011c8ea07
Kevin Gonzalez <kgonzalez@greenmountaintechnology.com>
2019-10-03 16:04:44 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
 
 function App() {
+  const [userSelection, setSelection] = useState(200);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <form>
+          <input type="text" placeholder="What broke now?" onChange={e => setSelection(Number(e.target.value))}/>
+        </form>
+      </div>
+
+      <div>
+        <img src={`https://http.cat/${userSelection}.jpg`} alt="Movie Poster"/>
+      </div>
+    </React.Fragment>
   );
 }
 
